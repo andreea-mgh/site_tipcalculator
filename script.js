5e3586d710f6b44e721a185393e5b2eaef9ff964
Andreea Megherlich <andreea.meghe@gmail.com>
2024-09-30 20:36:17 +0300

    fixed decimals

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 function calculateTip() {
     const amount = parseFloat(document.getElementById('amount').value);
     
-    if( document.querySelector('input[name="tipMethod"]:checked').value == "default" ) {
+    if ( document.querySelector('input[name="tipMethod"]:checked').value == "default" ) {
         const tip = amount * parseFloat(document.getElementById('tipPercentage').value);
         const total = amount + tip;
         
         document.getElementById('result').innerHTML = `
-        Bacșiș: ${tip} RON <br>
-        Total: ${total} RON
+        Bacșiș: ${tip.toFixed(2)} RON <br>
+        Total: ${total.toFixed(2)} RON
         `;
     }
-    else if( document.querySelector('input[name="tipMethod"]:checked').value == "custom1" ) {
+    else if ( document.querySelector('input[name="tipMethod"]:checked').value == "custom1" ) {
         const tip = amount * parseFloat(document.getElementById('customPercent').value) / 100;
         const total = amount + tip;
         
         document.getElementById('result').innerHTML = `
-        Bacșiș: ${tip} RON <br>
-        Total: ${total} RON
+        Bacșiș: ${tip.toFixed(2)} RON <br>
+        Total: ${total.toFixed(2)} RON
         `;
     }
-    else if( document.querySelector('input[name="tipMethod"]:checked').value == "custom2" ) {
+    else if ( document.querySelector('input[name="tipMethod"]:checked').value == "custom2" ) {
         const tip = parseFloat(document.getElementById('customAmount').value);
+        const tipPercentage = tip * 100 / amount;
         const total = amount + tip;
         
         document.getElementById('result').innerHTML = `
-        Bacșiș: ${tip} RON (${tip*100/total}%) <br>
-        Total: ${total} RON
+        Bacșiș: ${tip.toFixed(2)} RON (${tipPercentage.toFixed(2)}%) <br>
+        Total: ${total.toFixed(2)} RON
         `;
     }
 }
