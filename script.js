063fd2877cb02756f8975004c02ddb2cef0601e7
Andreea Megherlich <andreea.meghe@gmail.com>
2024-09-30 20:32:52 +0300

    custom percent and value

--- script.js
+++ script.js
@@ -10,4 +10,22 @@ function calculateTip() {
         Total: ${total} RON
         `;
     }
+    else if( document.querySelector('input[name="tipMethod"]:checked').value == "custom1" ) {
+        const tip = amount * parseFloat(document.getElementById('customPercent').value) / 100;
+        const total = amount + tip;
+        
+        document.getElementById('result').innerHTML = `
+        Bacșiș: ${tip} RON <br>
+        Total: ${total} RON
+        `;
+    }
+    else if( document.querySelector('input[name="tipMethod"]:checked').value == "custom2" ) {
+        const tip = parseFloat(document.getElementById('customAmount').value);
+        const total = amount + tip;
+        
+        document.getElementById('result').innerHTML = `
+        Bacșiș: ${tip} RON (${tip*100/total}%) <br>
+        Total: ${total} RON
+        `;
+    }
 }
